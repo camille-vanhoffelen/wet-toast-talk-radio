dff78efc7b1e82467bbb437a2fbd41609e5cc1be
dskart <dskart11@gmail.com>
2023-06-06 15:50:45 -0400

    feat: add local stack and S3MediaStore implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,22 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--integration",
+        "--intergration",
         action="store_true",
         default=False,
         help="run integration tests",
     )
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "intergration: mark test as intergration test")
+
+
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--integration"):
-        # --integration given in cli: do not skip integration tests
+    if config.getoption("--intergration"):
+        # --intergration given in cli: do not skip intergration tests
         return
-    skip_integration = pytest.mark.skip(reason="need --integration low option to run")
+    skip_integration = pytest.mark.skip(reason="need --intergration low option to run")
     for item in items:
         if "integration" in item.keywords:
             item.add_marker(skip_integration)
