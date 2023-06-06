0efacf49268a743a7dd303c225f7e60664d033a6
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-06 22:35:26 +0200

    feat: better working version for scriptwriter

--- wet_toast_talk_radio/scriptwriter/prompts.py
+++ wet_toast_talk_radio/scriptwriter/prompts.py
@@ -3,6 +3,7 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     SystemMessagePromptTemplate,
 )
+from langchain.schema import BaseOutputParser
 
 
 def generate_guest_generation_template(polarity: str):
@@ -12,12 +13,48 @@ def generate_guest_generation_template(polarity: str):
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     return ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
+
 def generate_script_generation_template():
-    system_template = "You are an edgy, satirical writer. You write dialogues in the following format:\nX: Did you eat the Doritos?\nY: No X, I didn't.\nX: I bet you did.\n"
-    human_template = "Here are the descriptions of two characters: {in_favor_guest} and {against_guest}. Imagine that these two characters are discussing the pros and cons of {topic}. They are stubborn, emotional, and stuck in disagreement. The conversation is chaotic and they cut eachother off often. Now generate a dialogue for this conversation of roughly 2000 words."
+    system_template = "You are an edgy, satirical writer. {format_instructions}"
+    human_template = "Here are the descriptions of two characters: {in_favor_guest} and {against_guest}. Imagine that these two characters are discussing the pros and cons of {topic}. They are stubborn, emotional, and stuck in disagreement. The conversation is chaotic. They cut eachother off often, and still disagree at the end. Now generate a dialogue for this conversation of roughly 2000 words."
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    return ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    return ChatPromptTemplate(messages=[system_message_prompt, human_message_prompt],
+                              input_variables=["in_favor_guest", "against_guest"],
+                              partial_variables={"format_instructions": ScriptOutputParser().get_format_instructions()})
+
+
+class ScriptOutputParser(BaseOutputParser):
+    """Class to parse the output of an LLM call to a script."""
+    n_speakers = 2
+
+    @property
+    def _type(self) -> str:
+        return "str"
+
+    def get_format_instructions(self) -> str:
+        return (
+            "Your response should be a dialogue in the following format:\n\n"
+            "X: Hi, how are you?\n\n"
+            "Y: I'm good, thanks. And you?\n\n"
+            "X: I'm pretty good.\n\n"
+        )
+
+    def parse(self, script: str) -> str:
+        """Parse the output of an LLM call."""
+        script = script.strip()
+        lines = script.split("\n\n")
+        speakers = set()
+        clean_lines = []
+        for line in lines:
+            assert ":" in line
+            speaker, text = line.split(":")
+            speakers.add(speaker.strip().lower())
+            clean_lines.append(line.strip())
+        assert len(speakers) == self.n_speakers, f"Expected {self.n_speakers} speakers, but got: {len(speakers)}"
+        clean_script = "\n".join(clean_lines)
+        return clean_script
+
 
 IN_FAVOR_GUEST = generate_guest_generation_template(polarity="in favor")
 AGAINST_GUEST = generate_guest_generation_template(polarity="against")
--- wet_toast_talk_radio/scriptwriter/the_great_debate.py
+++ wet_toast_talk_radio/scriptwriter/the_great_debate.py
@@ -1,40 +1,22 @@
-from typing import Any, Dict, List
+from typing import Any
 
-from langchain import LLMChain
+import structlog
 from langchain.base_language import BaseLanguageModel
 from langchain.callbacks.manager import (
     AsyncCallbackManagerForChainRun,
     CallbackManagerForChainRun,
 )
-from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
 from langchain.chains.base import Chain
 from langchain.prompts import ChatPromptTemplate
-from pydantic import Extra
 
 from wet_toast_talk_radio.scriptwriter import prompts
+from wet_toast_talk_radio.scriptwriter.prompts import ScriptOutputParser
 
 IN_FAVOR_GUEST_KEY = "in_favor_guest"
 AGAINST_GUEST_KEY = "against_guest"
 
-
-class FakeChain(Chain):
-    chain_1: LLMChain
-    chain_2: LLMChain
-
-    @property
-    def input_keys(self) -> List[str]:
-        # Union of the input keys of the two chains.
-        all_input_vars = set(self.chain_1.input_keys).union(set(self.chain_2.input_keys))
-        return list(all_input_vars)
-
-    @property
-    def output_keys(self) -> List[str]:
-        return ["concat_output"]
-
-    def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
-        output_1 = self.chain_1.run(inputs)
-        output_2 = self.chain_2.run(inputs)
-        return {"concat_output": output_1 + output_2}
+logger = structlog.get_logger()
 
 
 class GuestGenerationChain(Chain):
@@ -50,24 +32,12 @@ class GuestGenerationChain(Chain):
     in_favor_guest_output_key: str = IN_FAVOR_GUEST_KEY
     against_guest_output_key: str = AGAINST_GUEST_KEY
 
-    class Config:
-        """Configuration for this pydantic object."""
-
-        extra = Extra.forbid
-        arbitrary_types_allowed = True
-
     @property
     def input_keys(self) -> list[str]:
-        """Will be whatever keys the prompt expects.
-
-        """
         return [self.input_key]
 
     @property
     def output_keys(self) -> list[str]:
-        """Will always return text key.
-
-        """
         return [self.in_favor_guest_output_key, self.against_guest_output_key]
 
     def _call(
@@ -75,29 +45,37 @@ class GuestGenerationChain(Chain):
             inputs: dict[str, Any],
             run_manager: CallbackManagerForChainRun | None = None,
     ) -> dict[str, str]:
+        logger.info("Starting guest generation chain")
+        logger.debug("Guest generation chain inputs", inputs=inputs)
+
         in_favor_prompt_value = self.in_favor_prompt.format_prompt(**inputs)
         against_prompt_value = self.against_prompt.format_prompt(**inputs)
 
         in_favor_guest = self.llm.generate_prompt(
-            [in_favor_prompt_value],
+            prompts=[in_favor_prompt_value],
             callbacks=run_manager.get_child() if run_manager else None
         )
 
         against_guest = self.llm.generate_prompt(
-            [against_prompt_value],
+            prompts=[against_prompt_value],
             callbacks=run_manager.get_child() if run_manager else None
         )
 
-        # TODO remove
-
-        return {self.in_favor_guest_output_key: in_favor_guest.generations[0][0].text,
-                self.against_guest_output_key: against_guest.generations[0][0].text}
+        output = {self.in_favor_guest_output_key: in_favor_guest.generations[0][0].text,
+                  self.against_guest_output_key: against_guest.generations[0][0].text}
+        
+        logger.info("Finished guest generation chain")
+        logger.debug("Guest generation chain output", output=output)
+        return output
 
     async def _acall(
             self,
             inputs: dict[str, Any],
             run_manager: AsyncCallbackManagerForChainRun | None = None,
     ) -> dict[str, str]:
+        logger.info("Starting guest generation chain")
+        logger.debug("Guest generation chain inputs", inputs=inputs)
+
         in_favor_prompt_value = self.in_favor_prompt.format_prompt(**inputs)
         against_prompt_value = self.against_prompt.format_prompt(**inputs)
 
@@ -110,24 +88,31 @@ class GuestGenerationChain(Chain):
             [against_prompt_value],
             callbacks=run_manager.get_child() if run_manager else None
         )
-        return {self.in_favor_guest_output_key: in_favor_guest.generations[0][0].text,
-                self.against_guest_output_key: against_guest.generations[0][0].text}
+        output = {self.in_favor_guest_output_key: in_favor_guest.generations[0][0].text,
+                  self.against_guest_output_key: against_guest.generations[0][0].text}
 
-    @property
-    def _chain_type(self) -> str:
-        return "guest_generation_chain"
+        logger.info("Finished guest generation chain")
+        logger.debug("Guest generation chain output", output=output)
+        return output
+
 
 class ScriptGenerationChain(Chain):
+    """
+    Generates a radio script for "The Great Debate" show,
+    based on two guest descriptions and the show's topic.
+    """
+
     llm: BaseLanguageModel
     prompt: ChatPromptTemplate = prompts.SCRIPT
+    parser: ScriptOutputParser = ScriptOutputParser()
     output_key: str = "script"
 
     @property
-    def input_keys(self) -> List[str]:
+    def input_keys(self) -> list[str]:
         return self.prompt.input_variables
 
     @property
-    def output_keys(self) -> List[str]:
+    def output_keys(self) -> list[str]:
         return [self.output_key]
 
     def _call(
@@ -135,28 +120,73 @@ class ScriptGenerationChain(Chain):
             inputs: dict[str, Any],
             run_manager: CallbackManagerForChainRun | None = None,
     ) -> dict[str, str]:
+        logger.info("Starting script generation chain")
+        logger.debug("Script generation chain inputs", inputs=inputs)
+
         prompt_value = self.prompt.format_prompt(**inputs)
 
-        script = self.llm.generate_prompt(
-            [prompt_value],
+        result = self.llm.generate_prompt(
+            prompts=[prompt_value],
             callbacks=run_manager.get_child() if run_manager else None
         )
-        return {self.output_key: script.generations[0][0].text}
+        raw_script = result.generations[0][0].text
+        logger.debug("Parsing raw script", raw_script=raw_script)
+        script = self.parser.parse(raw_script)
+
+        logger.info("Finished script generation chain")
+        logger.debug("Script generation chain output", script=script)
+        return {self.output_key: script}
 
     async def _acall(
             self,
             inputs: dict[str, Any],
             run_manager: AsyncCallbackManagerForChainRun | None = None,
     ) -> dict[str, str]:
+        logger.info("Starting script generation chain")
+        logger.debug("Script generation chain inputs", inputs=inputs)
+
         prompt_value = self.prompt.format_prompt(**inputs)
 
-        script = await self.llm.agenerate_prompt(
-            [prompt_value],
+        result = await self.llm.agenerate_prompt(
+            prompts=[prompt_value],
             callbacks=run_manager.get_child() if run_manager else None
         )
-        return {self.output_key: script.generations[0][0].text}
+        raw_script = result.generations[0][0].text
+        logger.debug("Parsing raw script", raw_script=raw_script)
+        script = self.parser.parse(raw_script)
 
-class TheGreatDialogueChain(Chain):
-    # TODO implement SequentialChain of the above
-    pass
+        logger.info("Finished script generation chain")
+        logger.debug("Script generation chain output", script=script)
+        return {self.output_key: script}
 
+
+class TheGreatDebateChain(Chain):
+    """
+    Generates a radio script for "The Great Debate" show for a given topic.
+    Combines the guest generation and script generation chains.
+    """
+    chain: Chain
+
+    @property
+    def input_keys(self) -> list[str]:
+        return self.chain.input_keys
+
+    @property
+    def output_keys(self) -> list[str]:
+        return self.chain.output_keys
+
+    def _call(self, inputs: dict[str, Any], run_manager: CallbackManagerForChainRun | None = None, ) -> dict[str, Any]:
+        return self.chain._call(inputs=inputs, run_manager=run_manager)
+
+    async def _acall(self, inputs: dict[str, Any], run_manager: CallbackManagerForChainRun | None = None, ) -> dict[
+        str, Any]:
+        return await self.chain._acall(inputs=inputs, run_manager=run_manager)
+
+    @classmethod
+    def from_llm(cls, llm: BaseLanguageModel) -> "TheGreatDebateChain":
+        guest_generation_chain = GuestGenerationChain(llm=llm)
+        script_generation_chain = ScriptGenerationChain(llm=llm)
+        chain = SequentialChain(chains=[guest_generation_chain, script_generation_chain],
+                                input_variables=guest_generation_chain.input_keys,
+                                output_variables=guest_generation_chain.output_keys + script_generation_chain.output_keys)
+        return cls(chain=chain)
