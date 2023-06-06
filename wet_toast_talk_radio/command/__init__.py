369e9718031f2e44d4b24eb4378c36051766fd2f
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-07 00:22:40 +0200

    fix: maybe the last formatting commit

--- wet_toast_talk_radio/command/__init__.py
+++ wet_toast_talk_radio/command/__init__.py
@@ -1,6 +1,6 @@
 from wet_toast_talk_radio.command.audio_generator import audio_generator
-from wet_toast_talk_radio.command.scriptwriter import scriptwriter
 from wet_toast_talk_radio.command.disc_jokey import disc_jockey
 from wet_toast_talk_radio.command.root import root_cmd
+from wet_toast_talk_radio.command.scriptwriter import scriptwriter
 
 __all__ = ["root_cmd", "disc_jockey", "audio_generator", "scriptwriter"]
