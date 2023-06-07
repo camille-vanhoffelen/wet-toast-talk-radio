f176ae0faa5d4bf98b55f212a1bbfdefc2de846e
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-07 15:35:46 +0200

    feat: dependency injection for media store in scriptwriter

--- wet_toast_talk_radio/command/scriptwriter.py
+++ wet_toast_talk_radio/command/scriptwriter.py
@@ -3,6 +3,7 @@ import structlog
 
 from wet_toast_talk_radio.command.print_banner import print_banner
 from wet_toast_talk_radio.command.root import root_cmd
+from wet_toast_talk_radio.media_store import new_media_store
 from wet_toast_talk_radio.scriptwriter import Scriptwriter
 from wet_toast_talk_radio.scriptwriter.config import validate_config
 
@@ -28,9 +29,11 @@ def run(ctx: dict, topic: str):
     with TOPIC the topic of The Great Debate show.
     """
     root_cfg = ctx.obj["root_cfg"]
-    cfg = root_cfg.scriptwriter
+    sw_cfg = root_cfg.disc_jockey
+    ms_cfg = root_cfg.media_store
 
     logger.info("Starting scriptwriter", cfg=root_cfg.scriptwriter)
-    writer = Scriptwriter(cfg)
+    media_store = new_media_store(ms_cfg)
+    writer = Scriptwriter(cfg=sw_cfg, media_store=media_store)
 
     writer.run(topic=topic)
--- wet_toast_talk_radio/scriptwriter/scriptwriter.py
+++ wet_toast_talk_radio/scriptwriter/scriptwriter.py
@@ -1,6 +1,9 @@
+import uuid
+
 import structlog
 from langchain.chat_models import ChatOpenAI
 
+from wet_toast_talk_radio.media_store import MediaStore
 from wet_toast_talk_radio.scriptwriter.config import ScriptwriterConfig
 from wet_toast_talk_radio.scriptwriter.the_great_debate import TheGreatDebateChain
 
@@ -13,8 +16,9 @@ class Scriptwriter:
     llm_model: str = "gpt-3.5-turbo"
     llm_temperature: float = 0.9
 
-    def __init__(self, cfg: ScriptwriterConfig):
+    def __init__(self, cfg: ScriptwriterConfig, media_store: MediaStore):
         self._cfg = cfg
+        self._media_store = media_store
 
     def run(self, topic: str) -> None:
         llm = ChatOpenAI(
@@ -23,7 +27,11 @@ class Scriptwriter:
             temperature=self.llm_temperature,
         )
         chain = TheGreatDebateChain.from_llm(llm=llm)
+
         logger.info("Writing The Great Debate show...", topic=topic)
         outputs = chain(inputs={"topic": topic})
         script = outputs["script"]
         logger.info("Finished writing The Great Debate show", script=script)
+
+        show_name = uuid.uuid4().hex + ".txt"
+        self._media_store.upload_script_show(show_name=show_name, content=script)
