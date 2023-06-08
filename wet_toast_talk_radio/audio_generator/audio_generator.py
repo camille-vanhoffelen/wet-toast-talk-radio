c0e7a66ecc59e56993ce31b89cf5865342c9c560
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-08 15:04:09 +0200

    fix: gitignore output folder, and use in audio-generator

--- wet_toast_talk_radio/audio_generator/audio_generator.py
+++ wet_toast_talk_radio/audio_generator/audio_generator.py
@@ -29,7 +29,7 @@ class AudioGenerator:
         # download and load all models
         preload_models()
 
-        with Path("audio_generator/resources/noodle-nose.txt").open(
+        with Path("wet_toast_talk_radio/audio_generator/resources/noodle-nose.txt").open(
             "r", encoding="utf-8"
         ) as f:
             script = f.read()
@@ -50,14 +50,15 @@ class AudioGenerator:
 
         pieces = []
         for sentence in sentences:
-            logger.info(f"Generating audio for: {sentence}")
+            logger.info(f"Generating audio for: {sentence}", sentence=sentence)
             audio_array = generate_audio(sentence, history_prompt=SPEAKER)
             pieces += [audio_array, silence.copy()]
 
         end = time.perf_counter()
-        logger.info(f"Audio generation took {end - start:0.3f} seconds")
+        run_time = end - start
+        logger.info(f"Finished audio generation, run time: {run_time:.3f} s", run_time_in_s=run_time)
 
         audio_array = np.concatenate(pieces)
 
         uuid_str = str(uuid.uuid4())[:4]
-        write_wav(f"bark_generation_{uuid_str}.wav", SAMPLE_RATE, audio_array)
+        write_wav(Path.cwd() / "output" / f"bark_generation_{uuid_str}.wav", SAMPLE_RATE, audio_array)
