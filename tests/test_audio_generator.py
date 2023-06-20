f5e0df128c65d095490cc22724efdc28e20dc811
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-20 12:38:21 +0200

    feat: audio_generator integration test skeleton

---
+++ tests/test_audio_generator.py
@@ -0,0 +1,16 @@
+import pytest
+
+from wet_toast_talk_radio.audio_generator import AudioGenerator
+from wet_toast_talk_radio.audio_generator.config import AudioGeneratorConfig
+from wet_toast_talk_radio.media_store.media_store import ShowId
+
+
+class TestAudioGenerator:
+    @pytest.mark.integration()
+    def test_transcode(self, media_store, setup_bucket: dict[str, list[ShowId]]):
+        scripts = setup_bucket["script"]
+
+        cfg = AudioGeneratorConfig(use_s3_model_cache=False)
+
+        audio_generator = AudioGenerator(cfg)
+        audio_generator.run()
