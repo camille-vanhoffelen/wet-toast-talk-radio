538b4eb411318db3acaa09e19ffea99f9392897b
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-06-22 15:07:54 +0200

    fix: TDD audio-generator integration test

--- tests/test_audio_generator.py
+++ tests/test_audio_generator.py
@@ -3,7 +3,8 @@ import pytest
 from tests.conftests import (
     _clear_bucket,  # noqa: F401
     _clear_sqs,  # noqa: F401
-    media_store,  # noqa: F401
+    media_store,
+    message_queue,
     radio_operator,  # noqa: F401
     setup_bucket,  # noqa: F401
 )
@@ -14,16 +15,37 @@ from wet_toast_talk_radio.media_store.media_store import ShowId
 
 class TestAudioGenerator:
     @pytest.mark.integration()
-    def test_transcode(
-        self,
-        media_store,  # noqa: F811
-        setup_bucket: dict[str, list[ShowId]],  # noqa: F811
+    def test_audio_generator(
+        self, _clear_bucket, _clear_sqs, media_store, message_queue  # noqa: PT019, F811
     ):
-        scripts = setup_bucket["script"]
+        today = "2012-12-21"
+        show0 = ShowId(59, today)
+        show1 = ShowId(60, today)
+        shows = [show0, show1]
+        media_store.put_script_show(show0, "John: Toast is wet!")
+        media_store.put_script_show(show1, "Anna: No, it's not.")
+        message_queue.add_stream_shows(shows)
 
         cfg = AudioGeneratorConfig(use_s3_model_cache=False, use_small_models=True)
-        audio_generator = AudioGenerator(cfg=cfg, media_store=media_store)
+        audio_generator = AudioGenerator(
+            cfg=cfg, media_store=media_store, message_queue=message_queue
+        )
         audio_generator.run()
 
-        for s in scripts:
+        for s in shows:
             assert s in media_store.list_raw_shows()
+
+    @pytest.mark.integration()
+    def test_empty_queue(
+        self,
+        media_store,
+        message_queue,
+    ):
+        cfg = AudioGeneratorConfig(use_s3_model_cache=False, use_small_models=True)
+        audio_generator = AudioGenerator(
+            cfg=cfg, media_store=media_store, message_queue=message_queue
+        )
+        # TODO should exit
+        audio_generator.run()
+
+        assert not media_store.list_raw_shows()
