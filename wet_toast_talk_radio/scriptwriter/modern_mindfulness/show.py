65c1a1d4eabce68b0e1e28c3f4e3cf4a1b18f301
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-07-05 11:46:42 +0200

    feat: modern mindfulness now reads from situations

--- wet_toast_talk_radio/scriptwriter/modern_mindfulness/show.py
+++ wet_toast_talk_radio/scriptwriter/modern_mindfulness/show.py
@@ -1,4 +1,5 @@
 # ruff: noqa: E501
+import random
 
 import structlog
 from guidance import Program
@@ -8,14 +9,14 @@ from wet_toast_talk_radio.common.dialogue import Line
 from wet_toast_talk_radio.common.log_ctx import show_id_log_ctx
 from wet_toast_talk_radio.media_store import MediaStore
 from wet_toast_talk_radio.media_store.media_store import ShowId
+from wet_toast_talk_radio.scriptwriter.modern_mindfulness.situations import (
+    load_situations,
+)
 from wet_toast_talk_radio.scriptwriter.radio_show import RadioShow
-from wet_toast_talk_radio.scriptwriter.the_great_debate.topics import load_topics
-from wet_toast_talk_radio.scriptwriter.the_great_debate.traits import load_traits
 
 logger = structlog.get_logger()
 
-TOPICS = load_topics()
-TRAITS = load_traits()
+SITUATIONS = load_situations()
 
 PLAN_TEMPLATE = """{{#system~}}
 You are an edgy, satirical writer.
@@ -27,7 +28,7 @@ List the unlucky events one per line. Don't number them or print any other text,
 Use a casual tone and informal style.
 
 Here's an example:
-Situation: 
+Situation:
 Taking the metro to go to work.
 Events:
 Missing your stop.
@@ -37,7 +38,7 @@ Accidentally stepping on someone's foot.
 Making eye contact with your ex.
 
 Now generate a list of 5 unlucky yet harmless events.
-Situation: 
+Situation:
 {{situation}}.
 {{~/user}}
 {{#assistant~}}
@@ -55,9 +56,9 @@ Circumstances:
 Running late to go to yoga class for the third time this week.
 Having to pee.
 
-Now write this story in 2000 words and in the first person point of view. 
-Include all events and circumstances, in a order that makes chronological sense. 
-Describe each event in great detail, and focus on their most stressful and frustrating aspects. 
+Now write this story in 2000 words and in the first person point of view.
+Include all events and circumstances, in a order that makes chronological sense.
+Describe each event in great detail, and focus on their most stressful and frustrating aspects.
 End with an anxious cliffhanger.
 Use a casual tone and informal style. Make the story long, descriptive and relatable.
 {{~/user}}
@@ -69,7 +70,7 @@ You are now an enlightened spiritual guru.
 {{~/system}}
 {{#user~}}
 Your task is to turn the above story into a guided meditation.
-This is a mindfulness exercise combined with exposure therapy. 
+This is a mindfulness exercise combined with exposure therapy.
 Its purpose is for listeners to face and engage with their fears and anxieties in a safe environment.
 You should include all the stressful and frustrating descriptions of the story, and narrate them in extreme detail.
 You should encourage the listener to remain calm despite the challenges they encounter along the way.
@@ -90,9 +91,11 @@ class ModernMindfulness(RadioShow):
         self,
         llm: LLM,
         media_store: MediaStore,
+        situation: str | None = None,
     ):
         self._llm = llm
         self._media_store = media_store
+        self.situation = situation if situation else random.choice(SITUATIONS)
 
     @classmethod
     def create(cls, llm: LLM, media_store: MediaStore) -> "ModernMindfulness":
@@ -101,21 +104,14 @@ class ModernMindfulness(RadioShow):
     @show_id_log_ctx()
     async def awrite(self, show_id: ShowId) -> bool:
         logger.info(
-            "Async writing modern mindfulness",
+            "Async writing modern mindfulness", situation=self.situation
         )
 
+        # TODO generate circumstances
         plan = Program(text=PLAN_TEMPLATE, llm=self._llm, async_mode=True)
-        # situation = "taking the metro to go to work"
-        # TODO generate situations
-        # situation = "going to the hairdresser"
-        # situation = "cooking a meal for your children"
-        situation = "picking up a package from the post office"
-        # situation = "going to the supermarket to buy toilet paper"
-        written_plan = await plan(situation=situation)
+        written_plan = await plan(situation=self.situation)
         logger.debug("Written plan", debate=written_plan)
         logger.info("Finished writing Modern Mindfulness")
-        # logger.debug("Final script", content=lines)
-        # self._media_store.put_script_show(show_id=show_id, lines=lines)
         return True
 
     def _post_processing(self, script: str) -> list[Line]:
