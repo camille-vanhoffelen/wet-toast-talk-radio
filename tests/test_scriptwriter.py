ac417baf57c7dc9fc057be95d35fbcbe446e0f57
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-07-11 09:04:59 +0200

    feat: failing integration test for MQ accumulation

--- tests/test_scriptwriter.py
+++ tests/test_scriptwriter.py
@@ -67,6 +67,46 @@ class TestScriptwriter:
         assert len(media_store.list_script_shows()) == 0
         assert message_queue.poll_script_show() is None
 
+    @pytest.mark.integration()
+    def test_scriptwriter_daily_purge(  # noqa: PLR0913
+        self,
+        mocker,
+        _clear_bucket,  # noqa: PT019, F811
+        _clear_sqs,  # noqa: PT019, F811
+        media_store,  # noqa: F811
+        message_queue,  # noqa: F811
+        llm_config,
+        program,
+    ):
+        cfg = ScriptwriterConfig(llm=llm_config)
+        scriptwriter = Scriptwriter(
+            cfg=cfg,
+            media_store=media_store,
+            message_queue=message_queue,
+            program=program,
+        )
+        # day 1
+        scriptwriter.run()
+        n_shows = len(program)
+        assert len(media_store.list_script_shows()) == n_shows
+
+        # day 2
+        mocker.patch(
+            "wet_toast_talk_radio.scriptwriter.scriptwriter.get_offset_iso_utc_date",
+            return_value="2012-12-21",
+        )
+        scriptwriter2 = Scriptwriter(
+            cfg=cfg,
+            media_store=media_store,
+            message_queue=message_queue,
+            program=program,
+        )
+        scriptwriter2.run()
+        assert len(media_store.list_script_shows()) == n_shows
+
+        for _ in range(n_shows):
+            _poll_and_delete(message_queue)
+
 
 @pytest.fixture()
 def llm_config(program) -> LLMConfig:
