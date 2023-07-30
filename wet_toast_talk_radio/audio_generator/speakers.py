9ba7f7355fb5679b8fa7c0b4616fea970de1c1be
camille-vanhoffelen <camille-vanhoffelen@users.noreply.github.com>
2023-07-30 11:39:28 +0200

    feat: consistent speakers with voice cache

--- wet_toast_talk_radio/audio_generator/speakers.py
+++ wet_toast_talk_radio/audio_generator/speakers.py
@@ -1,9 +1,12 @@
 import random
 from pathlib import Path
 
+import structlog
 import torch
 
 from wet_toast_talk_radio.common.dialogue import Speaker
+
+logger = structlog.get_logger()
 
 HOST = "alex-brodie.pth"
 GUESTS = {
@@ -14,9 +17,29 @@ GUESTS = {
 VOICES_DIR = Path(__file__).parent / "resources" / "voices"
 
 
-def get_conditioning_latents(speaker: Speaker) -> tuple[torch.Tensor, torch.Tensor]:
+def init_voice_cache():
+    """Load all host voices into memory."""
+    logger.info("Initiliazing voice cache")
+    return {"Chris": load_conditioning_latent(VOICES_DIR / HOST)}
+
+
+def get_conditioning_latents(
+    speaker: Speaker, voice_cache: dict[str, tuple[torch.Tensor, torch.Tensor]]
+) -> tuple[torch.Tensor, torch.Tensor]:
     """For given speaker name and gender, return the conditioning latent to use for audio generation.
     This determines the voice. Host names are given consistent voices, while guest names are randomly assigned voices.
+    A voice cache is used to keep consistent guest voices, and only load them once.
     """
-    voice = HOST if speaker.name == "Chris" else random.choice(GUESTS[speaker.gender])
-    return torch.load(VOICES_DIR / voice)
+    if voice_cache is None:
+        raise ValueError("Voice cache must be initialized before use")
+    if speaker.name in voice_cache:
+        return voice_cache[speaker.name]
+    else:
+        voice = random.choice(GUESTS[speaker.gender])
+        conditioning_latents = load_conditioning_latent(VOICES_DIR / voice)
+        voice_cache[speaker.name] = conditioning_latents
+        return conditioning_latents
+
+
+def load_conditioning_latent(path: Path) -> tuple[torch.Tensor, torch.Tensor]:
+    return torch.load(path, map_location=torch.device("cpu"))
